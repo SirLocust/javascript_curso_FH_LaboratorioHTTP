375fd5ae735b8246276301de861446c3dbe304fb
sirlocust <jua56yu@hotmail.com>
2020-11-10 17:12:05 -0500

    clase 2 encadenando promesas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 
 const jokeUrl = 'https://api.chucknorris.io/jokes/random';
 
-fetch(jokeUrl).then((resp) =>{
-    resp.json().then( data =>{
-        console.log({...data.id,...data.value});
-    });
-})
+// fetch(jokeUrl).then((resp) =>{
+//     resp.json().then( data =>{
+//         console.log({...data.id,...data.value});
+//     });
+// })
 
-
+fetch( jokeUrl)
+        .then( resp => resp.json())
+        .then(console.log)
 
